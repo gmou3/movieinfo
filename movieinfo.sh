2b89609e566e0f4c3d374979510f2b8bcff1a02b
gmou3 <gmou3@hotmail.com>
2023-10-27 19:14:30 +0300

    Resize output based on terminal width
    ...and improve text formatting (dos to unix).

--- movieinfo.sh
+++ movieinfo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Text Formatting
+# Text formatting
 BOLD='\e[1m'
 RED='\e[1;31m'
 BLUE='\e[1;34m'
 NRM='\e[0m' # Normal
 
-# Read Movie and Search
-echo -en ${BOLD}Search${NRM}:' '
+# Read movie and search
+printf ${BOLD}Search${NRM}:' '
 read movie
 movie=$(echo $movie | sed -r 's/ /%20/g')
 content=$(wget https://www.rottentomatoes.com/search?search=$movie -qO -)
@@ -16,26 +16,29 @@ readarray titleList -t <<< $(echo ${movies[@]} | grep -oP '(?<=slot="title"> ).*
 readarray yearList -t <<< $(echo ${movies[@]} | grep -oP '(?<=releaseyear=").*?(?=")')
 readarray linkList -t <<< $(echo ${movies[@]} | grep -oP '(?<= </a> <a href=").*?(?=" class="unset" data-qa="info-name" slot="title">)')
 
-# Movie Choice Dialog
+# Movie choice dialog
 if [ ${#titleList[$i]} != 1 ]; then
-    echo -e ${BOLD}Choose movie${NRM}:
+    printf "${BOLD}Choose movie${NRM}:\n"
 else
-    echo -e ${BOLD}No results${NRM}
+    printf "${BOLD}No results${NRM}\n"
     exit
 fi
 for i in $(seq 0 7);
 do
     if [ ${#titleList[$i]} != 0 ]; then
-        echo -e '  '${BLUE}$i${NRM}. ${titleList[$i]} \(${yearList[$i]:=-}\) | sed 's/ )/)/'
+        printf "\t${BLUE}$i${NRM}. ${titleList[$i]} (${yearList[$i]:=-})" | tr -d '\n'
+        printf '\n'
     fi
 done
-echo -en ${BOLD}Choice \(${BLUE}0${NRM}${BOLD}\)${NRM}:' '
+printf "${BOLD}Choice (${BLUE}0${NRM}${BOLD})${NRM}: "
 read choice
 
-# Retrieve Chosen Movie Info
-content=$(wget ${linkList[choice]} -qO -)
-thumbnail=$(echo $content | grep -oP '(?<=<meta property="og:image" content=").*?(?=">)')
-wget -q $thumbnail -O /tmp/thumbnail.jpg
+# Retrieve chosen movie info
+content=$(wget ${linkList[$choice]} -qO -)
+
+img=$(echo $content | grep -oP '(?<=<meta property="og:image" content=").*?(?=">)')
+wget -q $img -O /tmp/img.jpg
+
 description=$(echo $content | grep -oP '(?<=description":").*?(?=",")' | head -1)
 language=$(echo $content | grep -oP '(?<=Original Language:</b> <span data-qa="movie-info-item-value">).*?(?=</span>)')
 director=$(echo $content | grep -oP '(?<="movie-info-director">).*?(?=</a>)')
@@ -45,32 +48,33 @@ tmp=$(echo $content | grep -oP '(?<=<score-board-deprecated).*?(?=</score-board-
 tomatoscore=$(echo $tmp | grep -oP '(?<=tomatometerscore=").*?(?=")')
 audiencescore=$(echo $tmp | grep -oP '(?<=audiencescore=").*?(?=")')
 
-# Print Chosen Movie Info
-script -q -c 'asciiart -c -w 25 /tmp/thumbnail.jpg' -O /dev/null >> /tmp/thumbnail.txt
-echo -e ${BOLD}${titleList[$choice]} \(${yearList[$choice]:=-}\)${NRM} | sed 's/ )/)/' >> /tmp/movieinfo.txt
-if [ "$description" = "Rotten Tomatoes every day." ]; then
-    echo '-' >> /tmp/movieinfo.txt
-else
-    echo ${description:=No description available} >> /tmp/movieinfo.txt
-fi
-echo ''
-echo -e ${BOLD}Visit${NRM}: ${linkList[choice]}
-echo ''
-echo '' >> /tmp/movieinfo.txt
-echo -e ${BOLD}Language${NRM}: ${language:=-} >> /tmp/movieinfo.txt
-echo -e ${BOLD}Director${NRM}: ${director:=-} >> /tmp/movieinfo.txt
-echo -e ${BOLD}Runtime${NRM}: ${runtime:=-} >> /tmp/movieinfo.txt
-echo -e ${BOLD}Genre${NRM}: ${genre:=-} >> /tmp/movieinfo.txt
-echo '' >> /tmp/movieinfo.txt
-echo -e ${RED}Tomatometer${NRM}: ${tomatoscore:=-} >> /tmp/movieinfo.txt
-echo -e ${BOLD}Audience Score${NRM}: ${audiencescore:=-} >> /tmp/movieinfo.txt
-fold -s -w55 /tmp/movieinfo.txt > /tmp/movieinfostd.txt
-if [ "$(cat /tmp/thumbnail.txt | grep asciiart)" ]; then # In case of asciiart error no image
-    cp /tmp/movieinfostd.txt /tmp/output.txt
-else
-    paste /tmp/thumbnail.txt /tmp/movieinfostd.txt | sed 's/\t/\t\t\t\t/' >> /tmp/output.txt
-fi
-cat /tmp/output.txt
+# Print chosen movie info
+termwidth=$(tput cols) # terminal width
+asciiwidth=$((27*$termwidth/100))
+txtwidth=$((6*$termwidth/10))
+script -q -c "asciiart -c -w $asciiwidth /tmp/img.jpg" -O /dev/null >> /tmp/imgdos
+tr -d '\r' < /tmp/imgdos > /tmp/img # dos to unix
 
-# Clear Cache
-rm /tmp/thumbnail.jpg /tmp/thumbnail.txt /tmp/movieinfo.txt /tmp/movieinfostd.txt /tmp/output.txt
+printf "${BOLD}${titleList[$choice]} (${yearList[$choice]:=-})${NRM}" | tr -d '\n' >> /tmp/mvinfo
+if [ "$description" = "Rotten Tomatoes every day." ]; then
+    printf "\n-" >> /tmp/mvinfo
+else
+    printf "\n${description:=No description available}\n" >> /tmp/mvinfo
+fi
+printf "\n${BOLD}Visit${NRM}: ${linkList[choice]}\n"
+printf "\n${BOLD}Language${NRM}: ${language:=-}\n" >> /tmp/mvinfo
+printf "${BOLD}Director${NRM}: ${director:=-}\n" >> /tmp/mvinfo
+printf "${BOLD}Runtime${NRM}: ${runtime:=-}\n" >> /tmp/mvinfo
+printf "${BOLD}Genre${NRM}: ${genre:=-}\n\n" >> /tmp/mvinfo
+printf "${RED}Tomatometer${NRM}: ${tomatoscore:=-}\n" >> /tmp/mvinfo
+printf "${BOLD}Audience Score${NRM}: ${audiencescore:=-}" >> /tmp/mvinfo
+fold -s -w $txtwidth /tmp/mvinfo > /tmp/mvinfostd
+if [ "$(cat /tmp/img | grep asciiart)" ]; then # In case of asciiart error no image
+    cp /tmp/mvinfostd /tmp/output
+else
+    paste /tmp/img /tmp/mvinfostd >> /tmp/output
+fi
+cat /tmp/output
+
+# Clear temporary files
+rm /tmp/img.jpg /tmp/img /tmp/imgdos /tmp/mvinfo /tmp/mvinfostd /tmp/output
